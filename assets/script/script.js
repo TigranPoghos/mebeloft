7fcdba0454afc2e7bace5a4c8feaba32a84b725c
TigranPoghos <tigran2002poghosyan@gmail.com>
2026-05-08 19:53:05 +0300

    1

--- assets/script/script.js
+++ assets/script/script.js
@@ -30,6 +30,38 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
 
+    var swiper = new Swiper(".mySwiper", {
+      navigation: {
+        nextEl: ".slider__button-right",
+        prevEl: ".slider__button-left",
+      },
+    });
+
+
+
+    (function faqAccordion() {
+        const faqItems = document.querySelectorAll('.faq__item');
+        if (!faqItems.length) return;
+        faqItems.forEach((faqItem) => {
+            const faqButton = faqItem.querySelector('.faq__item-head');
+            const faqBody = faqItem.querySelector('.faq__item-body');
+            if (!faqButton || !faqBody) return;
+            if (faqItem.classList.contains('active')) {
+                faqBody.style.maxHeight = faqBody.scrollHeight + 'px';
+            }
+            faqButton.addEventListener('click', () => {
+                faqItem.classList.toggle('active');
+
+                if (faqItem.classList.contains('active')) {
+                    faqBody.style.maxHeight = faqBody.scrollHeight + 'px';
+                } else {
+                    faqBody.style.maxHeight = null;
+                }
+            });
+        });
+    })();
+
+
     
 
 
